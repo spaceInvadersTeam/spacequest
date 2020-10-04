95e7b4a0995d0eb90f596ccf6d3d22677adbfac3
Lucas Baldoni <lucas.baldoni@brubank.com>
2020-10-04 00:55:26 -0300

    Add help texts

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:space_quest/localization/translations.dart';
 
 class HelpPage extends StatelessWidget {
@@ -8,15 +10,48 @@ class HelpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var translator = Translations.of(context);
     return Scaffold(
-      body:Container(
-        decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/F2.png"),
-     fit: BoxFit.fitWidth
-    ) ,
-    ),
-    ),)
-    ;
+      body: new Stack(
+          children: <Widget>[
+          Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/F2.png"),
+                fit: BoxFit.fitWidth
+              ) ,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.1,
+            width: MediaQuery.of(context).size.width,
+            child: Center (
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding:  EdgeInsets.fromLTRB(15, 15, 15, 15),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child : new RichText(text:  new TextSpan(
+                    style:  GoogleFonts.raleway( textStyle: new TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.black,
+                    )),
+                  children: <TextSpan>[
+                    new TextSpan( text: translator.text('help_text_1_1')),
+                    new TextSpan( text: translator.text('help_text_1_2'), style: const TextStyle(fontWeight: FontWeight.bold), ),
+                    new TextSpan( text: translator.text('help_text_1_3')),
+                    new TextSpan( text: "\n"+"\n"),
+                    new TextSpan( text: translator.text('help_text_1_4')),
+                    new TextSpan( text: "\n"+"\n"),
+                    new TextSpan( text: translator.text('help_text_1_5'), style: const TextStyle(fontWeight: FontWeight.bold), ),
+                ])
+                )
+              )
+            )
+          ),
+        ],
+      )
+    );
   }
 
 }
